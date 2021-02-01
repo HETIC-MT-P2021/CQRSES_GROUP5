f97b3220d72eb760a6943c602b938ab3f6288a40
Raineau Thomas <thomasraineau3@gmail.com>
2021-02-01 17:05:03 +0100

    Add: TODO in repo and order handler

--- domain/order/command.go
+++ domain/order/command.go
@@ -3,9 +3,9 @@ package domain_order
 import (
 	"errors"
 	"github.com/HETIC-MT-P2021/gocqrs/core/cqrs"
-	"github.com/HETIC-MT-P2021/gocqrs/database"
 	"github.com/HETIC-MT-P2021/gocqrs/helpers"
 	"github.com/HETIC-MT-P2021/gocqrs/models"
+	order_repository "github.com/HETIC-MT-P2021/gocqrs/repository/order"
 )
 
 type CreateOrderCommand struct {
@@ -21,25 +21,25 @@ type AddOrderLineCommand struct {
 type CreateOrderCommandHandler struct{}
 
 func (ch CreateOrderCommandHandler) Handle(command cqrs.CommandMessage) error {
-	db := database.DbConn
-	repository := models.Repository{Conn: db}
-
 	switch cmd := command.Payload().(type) {
 	case *CreateOrderCommand:
 		order := &models.Order{
-			TotalPrice: 230,
+			TotalPrice: 0,
 			Client:     cmd.Client,
 			Reference:  helpers.RandomString10(),
 			Lines:      []*models.OrderLine{},
 		}
-		order.NewOrder(order)
+		if err := order_repository.PersistOrder(order); err != nil {
+			return err
+		}
+
 	case *AddOrderLineCommand:
 		orderLine := &models.OrderLine{
 			Meal:    cmd.Meal,
 			Price:   cmd.Price,
 			IDOrder: cmd.IDOrder,
 		}
-		repository.AddOrderLine(orderLine)
+		order_repository.PersistOrderLine(orderLine)
 	default:
 		return errors.New("bad command type")
 	}
